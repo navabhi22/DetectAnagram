6fd036de13d60568e7561e6d4c34f8336e3a06aa
navabhi22 <navabhi22@gmail.com>
2022-03-24 09:43:13 +0530

    [Abhinav] Greeting message for Anagram detection program

---
+++ AnagramDetectionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnagramDetectionProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Anagram Detection Program");
+        }
+    }
+}
