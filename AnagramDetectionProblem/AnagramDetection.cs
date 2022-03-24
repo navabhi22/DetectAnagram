fafcfdbaf0c3cbf463158c0628492adb83da7966
navabhi22 <navabhi22@gmail.com>
2022-03-24 09:46:22 +0530

    [Abhinav] Added method to detect anagram

--- AnagramDetectionProblem/AnagramDetection.cs
+++ AnagramDetectionProblem/AnagramDetection.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace AnagramDetectionProblem
 {
-    internal class AnagramDetection
+    public class AnagramDetection
     {
+        public static void DetectAnagram(string str1, string str2)
+        {
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string check1 = new string(ch1);
+            string check2 = new string(ch2);
+            if (check1 == check2)
+            {
+                Console.WriteLine("Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+        }
     }
 }
